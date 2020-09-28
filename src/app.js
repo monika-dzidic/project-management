d9d9e46d0e07a2167c52a2a4e6186c371b187b31
dzidic <monika.dzidic@mostar.evona.sk>
2020-09-28 22:57:39 +0200

    init

---
+++ src/app.js
@@ -0,0 +1,5 @@
+class App {
+    static init() { }
+}
+
+App.init();
---
+++ webpack.config.prod.js
@@ -0,0 +1,12 @@
+const path = require('path');
+
+// eslint-disable-next-line no-undef
+module.exports = {
+    mode: 'production',
+    entry: './src/app.js',
+    output: {
+        filename: 'app.js',
+        path: path.resolve(__dirname, 'assets', 'scripts'),
+        publicPath: 'assets/scripts/'
+    }
+}
