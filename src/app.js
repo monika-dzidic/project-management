85ca1a5ee179183d5ef5c346ed620927c837d312
dzidic <monika.dzidic@mostar.evona.sk>
2020-10-15 19:38:22 +0200

    v1

--- src/app.js
+++ src/app.js
@@ -1,5 +1,104 @@
+/*style*/
+import '../assets/styles/app.css';
+import 'material-icons/iconfont/material-icons.css';
+import 'typeface-montserrat';
+
+/*vendor*/
+import * as firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/database';
+
+/*services*/
+import AuthService from './services/auth.service';
+import NotifyService from './services/notify.service';
+import { toggleLoading } from './util/dom-helper.service';
+
+import { ItemTypeEnum } from './enums/enums';
+
 class App {
-    static init() { }
+    init() {
+        toggleLoading(true);
+
+        firebase.initializeApp({
+            apiKey: process.env.API_KEY,
+            authDomain: process.env.AUTH_DOMAIN,
+            databaseURL: process.env.DATABASE_URL,
+            projectId: process.env.PROJECT_ID,
+            storageBucket: process.env.STORAGE_BUCKET,
+            messagingSenderId: process.env.MESSAGING_SENDER_ID,
+            appId: process.env.APP_ID
+        });
+
+        NotifyService.initializeService();
+
+        this.authService = new AuthService();
+        this.authService.setLoginHandlerFunction(this.loggedInHandler.bind(this));
+
+        this.authService.isLoggedIn().then(response => {
+            if (response) {
+                this.loadHeaderModule();
+                this.loadListModule();
+            } else {
+                this.loadHeaderModule();
+                this.loadLoginModule();
+            }
+        });
+    }
+
+    loggedInHandler(loggedIn) {
+        if (loggedIn) {
+            if (this.loginModule) {
+                this.loginModule.removeLoginButtons();
+            }
+            this.listModule ? this.getLists() : this.loadListModule();
+            this.headerModule.createMenuIcon();
+        } else {
+            this.activeProjects.removeSection();
+            this.activeProjects = null;
+            this.finishedProjects.removeSection();
+            this.finishedProjects = null;
+            document.getElementById('projects-container').remove();
+
+            this.loginModule ? this.loginModule.createLoginButtons() : this.loadLoginModule();
+            this.headerModule.removeMenuIcon();
+        }
+    }
+
+    async loadListModule() {
+        this.listModule = await import('./components/list.js');
+        this.getLists();
+    }
+
+    getLists() {
+        this.activeProjects = new this.listModule.default(ItemTypeEnum.active());
+        this.finishedProjects = new this.listModule.default(ItemTypeEnum.finished());
+
+        const getActiveProjects = this.activeProjects.getProjects();
+        const getFinishedProjects = this.finishedProjects.getProjects();
+
+        Promise.all([getActiveProjects, getFinishedProjects]).then(() => {
+            const projectContainer = document.createElement('div');
+            projectContainer.id = 'projects-container';
+            projectContainer.insertAdjacentElement('afterbegin', this.activeProjects.section);
+            projectContainer.insertAdjacentElement('beforeend', this.finishedProjects.section);
+
+            this.activeProjects.setSwitchHandlerFunction(this.finishedProjects.addProject.bind(this.finishedProjects));
+            this.finishedProjects.setSwitchHandlerFunction(this.activeProjects.addProject.bind(this.activeProjects));
+
+            document.body.insertAdjacentElement('beforeend', projectContainer);
+
+            toggleLoading(false);
+        });
+    }
+
+    async loadLoginModule() {
+        this.loginModule = new (await import('./components/login.js')).default(this.authService);
+        this.loginModule.getSVGs();
+    }
+
+    async loadHeaderModule() {
+        this.headerModule = new (await import('./components/header.js')).default(this.authService);
+    }
 }
 
-App.init();
+new App().init();
--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,12 +1,45 @@
+const { merge } = require('webpack-merge');
+const common = require('./webpack.config.common');
 const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-// eslint-disable-next-line no-undef
-module.exports = {
-    mode: 'production',
-    entry: './src/app.js',
-    output: {
-        filename: 'app.js',
-        path: path.resolve(__dirname, 'assets', 'scripts'),
-        publicPath: 'assets/scripts/'
-    }
-}
+module.exports = merge(common, {
+  mode: 'production',
+  output: {
+    filename: 'main-[contentHash].bundle.js',
+    path: path.resolve(__dirname, 'public'),
+    publicPath: ''
+  },
+  optimization: {
+    minimizer: [
+      new OptimizeCssAssetsPlugin(),
+      new TerserPlugin(),
+      new HtmlWebpackPlugin({
+        template: 'index.html',
+        minify: {
+          removeAttributeQuotes: true,
+          collapseWhitespace: true,
+          removeComments: true
+        }
+      })
+    ]
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({
+      filename: '[name].[contentHash].css'
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader']
+      }
+    ]
+  }
+});
